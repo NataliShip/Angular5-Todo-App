e134ccd238d8b7f9c567da5ab22e2472b566dd7a
NataliShip <Natulechka_m.25@mail.ru>
2017-11-15 19:31:10 +0300

    Добавление основного функционала приложения. Использование модуля форм, двусторонней привязки данных

--- Todo/src/app/home/home.component.ts
+++ Todo/src/app/home/home.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    itemCount: number = 4;
+    goalText: string = '';
+    goals =['Изучить Angular'];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+        this.itemCount = this.goals.length;
+    }
+
+    addItem(){
+        this.goals.push(this.goalText);
+        this.goalText = '';
+        this.itemCount = this.goals.length;
+    }
 
 }
