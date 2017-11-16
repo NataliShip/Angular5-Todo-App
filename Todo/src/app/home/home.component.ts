5c7ae3d4b458091f7e1814aa7a685628e7268416
NataliShip <Natulechka_m.25@mail.ru>
2017-11-16 18:50:14 +0300

    Добавление анимаций и модуля angular/animations

--- Todo/src/app/home/home.component.ts
+++ Todo/src/app/home/home.component.ts
@@ -1,10 +1,32 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {trigger, style, transition, animate, keyframes, query, stagger} from '@angular/animations'
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
-    encapsulation: ViewEncapsulation.None
+    encapsulation: ViewEncapsulation.None,
+    animations: [
+        trigger('goals', [
+            transition('* => *', [
+                query(':enter', style({ opacity:0 }), { optional: true }),
+
+                query(':enter', stagger('500ms', [
+                    animate('.5s ease-in', keyframes([
+                        style({ opacity: 0, width:'30%', offset:0}),
+                        style({ opacity: .5, width:'50%', offset:.3}),
+                        style({ opacity: 1, width:'100%', offset:1}),
+                    ]))]), { optional: true }),
+
+                query(':leave', stagger('500ms', [
+                    animate('.5s ease-in', keyframes([
+                        style({ opacity: 1, width:'100%', offset:0}),
+                        style({ opacity: .5, width:'50%', offset:.3}),
+                        style({ opacity: 0, width:'30%', offset:1}),
+                    ]))]), { optional: true })
+            ])
+        ])
+    ]
 })
 export class HomeComponent implements OnInit {
 
@@ -20,9 +42,15 @@ export class HomeComponent implements OnInit {
     }
 
     addItem(){
-        this.goals.push(this.goalText);
-        this.goalText = '';
-        this.itemCount = this.goals.length;
+        if (this.goalText !=''){
+            this.goals.push(this.goalText);
+            this.goalText = '';
+            this.itemCount = this.goals.length;
+        }
+    }
+
+    removeItem(i){
+        this.goals.splice(i, 1);
     }
 
 }
